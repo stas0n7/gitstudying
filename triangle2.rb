dd9e9e5c0409104cac873c68f0923dfc67b82553
stas0n7 <r14war@gmail.com>
2019-08-02 21:00:42 +0300

    in triangle2 changed > to >= 7,9,11,13,15,17 strings and add string 20

--- triangle2.rb
+++ triangle2.rb
@@ -1,19 +1,20 @@
 puts "Укажите длинну первой стороны треуголника"
-sta = gets
+sta = gets.chomp
 puts "Укажите длинну второй стороны треуголника"
-stb = gets
+stb = gets.chomp
 puts "Укажите длинну третьей стороны треуголника"
-stc = gets
-if sta.to_f > stb.to_f && sta.to_f > stc.to_f && sta.to_f**2 == stb.to_f**2 + stc.to_f**2
-    puts "Треугольник прямоуголный"
-elsif sta.to_f > stb.to_f && sta.to_f > stc.to_f && sta.to_f**2 != stb.to_f**2 + stc.to_f**2
+stc = gets.chomp
+if sta.to_f >= stb.to_f && sta.to_f >= stc.to_f && sta.to_f**2 == stb.to_f**2 + stc.to_f**2
+    puts "Треугольник прямоугольный"
+elsif sta.to_f >= stb.to_f && sta.to_f >= stc.to_f && sta.to_f**2 != stb.to_f**2 + stc.to_f**2
     puts "Треугоник не прямоугольный"
-elsif stb.to_f > sta.to_f && stb.to_f > stc.to_f && stb.to_f**2 == sta.to_f**2 + stc.to_f**2
+elsif stb.to_f >= sta.to_f && stb.to_f >= stc.to_f && stb.to_f**2 == sta.to_f**2 + stc.to_f**2
     puts "Треуголник прямоугольный"
-elsif stb.to_f > sta.to_f && stb.to_f > stc.to_f && stb.to_f**2 != sta.to_f**2 + stc.to_f**2
+elsif stb.to_f >= sta.to_f && stb.to_f >= stc.to_f && stb.to_f**2 != sta.to_f**2 + stc.to_f**2
     puts "Треугольник не прямоугольный"
-elsif stc.to_f > sta.to_f && stc.to_f > stb.to_f && stc.to_f**2 == sta.to_f**2 + stb.to_f**2
+elsif stc.to_f >= sta.to_f && stc.to_f >= stb.to_f && stc.to_f**2 == sta.to_f**2 + stb.to_f**2
     puts "Треуголник прямоугольный"
-elsif stc.to_f > sta.to_f && stc.to_f > stb.to_f && stc.to_f**2 != sta.to_f**2 + stb.to_f**2
+elsif stc.to_f >= sta.to_f && stc.to_f >= stb.to_f && stc.to_f**2 != sta.to_f**2 + stb.to_f**2
     puts "Треугольник не прямоугольный"
 end
+puts "Треуголник равнобедренный" if sta.to_f == stb.to_f || sta.to_f == stc.to_f || stb.to_f == stc.to_f
