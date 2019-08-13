9d2d1364a911c204444e3296fcc26d659b915eb0
stas0n7 <r14war@gmail.com>
2019-08-14 00:50:36 +0300

    in file 'fibonacci' , changed [-1] to .last on 2,3,5,6 strings

--- lesson2/fibonacci.rb
+++ lesson2/fibonacci.rb
@@ -1,8 +1,8 @@
-fi = [0,1]
-while fi[-1] < 100 
-    fi << fi[-1] + fi[-2]
+fi = [0, 1]
+while fi.last < 100
+  fi << fi.last + fi[-2]
 end
-if fi[-1] > 100
-    fi.delete_at(-1)
+if fi.last > 100
+  fi.pop
 end
 puts fi
