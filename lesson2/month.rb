b37adb5f11026a1d284f1a6cd9e9973afa5e1fc6
stas0n7 <r14war@gmail.com>
2019-08-13 15:55:07 +0300

    aligned the hash and on string 14 changed x,y to key, value

--- lesson2/month.rb
+++ lesson2/month.rb
@@ -1,8 +1,15 @@
-month = {january: 31 , february: 28 , march: 31 , april: 30 , may: 31 , june: 30,
-july: 31, august: 30 , september: 30, october: 31,november: 29, december:31}
-month30 = month.select {|x,y| y == 30}
+month = { january: 31,
+          february: 28,
+          march: 31,
+          april: 30,
+          may: 31,
+          june: 30,
+          july: 31,
+          august: 30,
+          september: 30,
+          october: 31,
+          november: 29,
+          december: 31 }
+
+month30 = month.select { |key, value| value == 30 }
 puts month30
-
-
-
-
