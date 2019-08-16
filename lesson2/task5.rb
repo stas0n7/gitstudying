7f6fd081db50218905710f1793bb044359528ede
stas0n7 <r14war@gmail.com>
2019-08-16 14:44:48 +0300

    redid 50% of task , i dont how correct wroting commit in this situation

--- lesson2/task5.rb
+++ lesson2/task5.rb
@@ -1,25 +1,21 @@
-#Заданы три числа, которые обозначают число, месяц, год. Найти порядковый номер даты, начиная отсчет с начала года.
-#Учесть что год может быть высокосным.
-puts "Введите день"
-date = gets.to_i
-puts "Введите номер месяца"
-number_month = gets.to_i
-puts "Введите год"
-year = gets
-if year.to_i % 4 == 0
-  year = :leap
-  months = [31, 29, 31, 30, 31, 30, 31, 30, 30, 31, 29, 31]
-  puts "Год высокосный"
+puts "type a day"
+day = gets.to_i
+puts "type number of month"
+month = gets.to_i
+puts "type year"
+year = gets.to_i
+months = [31, 28, 31, 30, 31, 30, 31, 30, 30, 31, 29, 31]
+if year % 4 == 0
+  months[1] = 29
+  puts "leap year"
 else
-  year = :not_leap
-  months = [31, 28, 31, 30, 31, 30, 31, 30, 30, 31, 29, 31]
-  puts "Год не высокосный"
+  puts "year is not leap"
 end
-days = 0
-i = 0
-while i < number_month
-  days += months[i]
-  i += 1
+days_count = 0
+index = 0
+while index < month
+  days_count += months[index]
+  index += 1
 end
-count = days - months[i - 1]
-puts "#{count + date} день по счёту "
+day_of_year = days_count - months[index - 1]
+puts "#{day_of_year + day} day"
