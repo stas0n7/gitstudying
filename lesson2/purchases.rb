0f854f21c77721c375153f9c41b4b9cfd1b9d2e6
stas0n7 <r14war@gmail.com>
2019-09-06 14:26:37 +0300

    improved 'purchases.rb' file according code review comment

--- lesson2/purchases.rb
+++ lesson2/purchases.rb
@@ -1,15 +1,18 @@
 purchases = Hash.new
+puts "Please , populate product collection"
 loop do
   puts "Name of product :"
   name = gets.chomp.downcase
-  if name == "stop" || name == "стоп"
-    break
-  end
   puts "Enter a price"
   price = gets.to_f
   puts "How much did you buy ?"
   how_many_bought = gets.to_f
   purchases[name] = { price => how_many_bought }
+  puts "you want add another one product? 'y' or 'no'"
+  continue = gets.chomp.downcase
+  if continue == "no" || continue == "not"
+    break
+  end
 end
 puts purchases
 total = []
