2d7e26bc71824d7293e28ba2dd89fb21f6ff3ccf
stas0n7 <r14war@gmail.com>
2019-10-05 13:56:34 +0300

    Complete full lesson7

---
+++ lesson7/station.rb
@@ -0,0 +1,87 @@
+class Station
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @train_list = []
+    @pas = 0
+    @cargo = 0
+    validate!
+  rescue ArgumentError => e
+    puts e.inspect
+  end
+
+  #lesson 7 , task 1 on 15-27 strings
+  def block_method(&block)
+    if @train_list.empty?
+      puts "No trains on the station"
+    elsif block_given?
+      for train in @train_list
+        block.call(train)
+      end
+    else
+      puts "No blocks"
+    end
+  end
+
+  #block_method { |x| puts "#{x} I am train on the station" }
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  def self.all
+    ObjectSpace.each_object(self) { |station| puts station.name }
+  end
+
+  def take(train)
+    if @train_list.include? train
+      puts "This train already on the station"
+    else
+      puts "On the station #{@name} arrived #{train.type} train #{train.number}, with #{train.carriages_count} carriages"
+      @train_list << train
+      case train.type
+      when :pas then @pas += 1
+      when :cargo then @cargo += 1
+      end
+    end
+  end
+
+  def take_off(train)
+    unless @train_list.include? train
+      puts "This train is not at this station"
+    else
+      puts "From station #{@name} departed #{train.type} train #{train.number} , with #{train.carriages_count} carriages"
+      @train_list.delete(train)
+      case train.type
+      when :pas then @pas -= 1
+      when :cargo then @cargo -= 1
+      end
+    end
+  end
+
+  def list_type
+    puts "passenger trains - #{@pas} :"
+    @train_list.find_all { |train| train.type == :pas }.each { |train| puts "#{train.type} train #{train.number}" }
+    puts "Cargo trains - #{@cargo} :"
+    @train_list.find_all { |train| train.type == :cargo }.each { |train| puts "#{train.type} train #{train.number}" }
+  end
+
+  def list
+    if @train_list.empty?
+      puts "No trains on this station yet"
+    else
+      puts "On the station now :"
+      @train_list.find_all { |train| puts "#{train.type} train #{train.number} , with #{train.carriages_count} carriages" }
+    end
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "Station name can't be less then 3 symbols" if @name.length < 3
+    true
+  end
+end
